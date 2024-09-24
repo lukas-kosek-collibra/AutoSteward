d7cef8a18f19c4a412bcf75c9488bcbd020d278f
lukas-kosek-collibra <lukas-kosek-collibra@users.noreply.github.com>
2024-09-24 21:13:42 +0000

    deploy: 00040a91bc50a3fa2fe436965db1d39d5335bb24

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from backend.routes import regex
-
-app = FastAPI()
-
-app.include_router(regex.router)
--- backend/run.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-from core.config import load_google_credentials
-
-if __name__ == "__main__":
-    load_google_credentials()
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
