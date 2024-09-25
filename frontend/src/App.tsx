99ed0d4c5d5dc32253d48446c37aeeda1e23850f
Maurice Kelly <139386288+Mkelly-Collibra@users.noreply.github.com>
2024-09-25 15:18:39 -0500

    Added in routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import { RouterProvider } from "react-router-dom";
 import { DataSummary } from "./components/DataSummary/DataSummary";
 import { Navigation } from "./components/Navigation/Navigation";
 import { Flex } from "@chakra-ui/react";
+import { router } from "./routes/router";
 
 function App() {
   return (
     <Flex direction={"column"} flex={1}>
       <Navigation />
-      <DataSummary />
+      <RouterProvider router={router} />
     </Flex>
   );
 }
---
+++ frontend/src/routes/route.tsx
@@ -0,0 +1,8 @@
+import { DataSummary } from "../components/DataSummary/DataSummary";
+
+export const ROUTES = [
+  {
+    path: "/AutoSteward/",
+    element: <DataSummary />,
+  },
+];
