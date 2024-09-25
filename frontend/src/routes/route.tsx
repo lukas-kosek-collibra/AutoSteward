211791bd0fcaa8d5b322484a463f2d8389026d3a
Maurice Kelly <139386288+Mkelly-Collibra@users.noreply.github.com>
2024-09-25 17:00:36 -0500

    policy page

--- frontend/src/routes/route.tsx
+++ frontend/src/routes/route.tsx
@@ -1,8 +1,14 @@
+import { path } from "framer-motion/client";
 import { DataSummary } from "../components/DataSummary/DataSummary";
+import { Policy } from "../components/Policy/policy";
 
 export const ROUTES = [
   {
     path: "/AutoSteward/",
     element: <DataSummary />,
+  },
+  {
+    path: "/AutoSteward/policy",
+    element: <Policy />,
   },
 ];
